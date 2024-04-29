43e213e8b82d2c5b8dc801d78c3f4fb08987c8e0
avilrod3004 <avilrod3004@g.educaand.es>
2024-04-29 09:34:47 +0200

    imagenes ejemplo 1 :)

---
+++ src/main/kotlin/images_and_icon_manipulation/images_from_resources/Main.kt
@@ -0,0 +1,15 @@
+package images_and_icon_manipulation.images_from_resources
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main() = singleWindowApplication {
+    Image(
+        painter = painterResource("sample.png"),
+        contentDescription = "Sample",
+        modifier = Modifier.fillMaxSize()
+    )
+}
